86266bda9ad82c3f48c057f5ba33d82602f179d3
WellingtonJr <wferreira.bm13@gmail.com>
2022-07-13 00:12:50 -0300

    definicao e tipos de variaveis personalizados

--- app.ts
+++ app.ts
@@ -1,26 +1,21 @@
-let btn = document.getElementById('btn1');
-let input1 = document.getElementById('input-1') as HTMLInputElement;
-let input2 = document.getElementById('input-2') as HTMLInputElement;
+/**
+ * Quando tipamos podemos passar mais de um tipo, como no caso abaixo em que cada parâmetro contém o tipo input, que é igual aos tipos number ou string
+ * No exemplo abaixo também ocorre um tratamento interno na função para que ela lide com os diferentes tipos de cada parâmetro
+*/
 
+// Também é possível criar tipos! E combinar tipos com esses tipos!
+type input = number | string;
 
-function somarNumeros(n1: number, n2: number, devePrintar: boolean, frase: string){
-    let resultado = n1 + n2;
-    if(devePrintar){
-        console.log(frase + resultado);
+function juntar(input1: input, input2: input) {
+    let resultado: input;
+    if (typeof input1 === 'string' || typeof input2 === 'string') {
+        resultado = input1.toString() + input2.toString();
+    } else {
+        resultado = input1 + input2
     }
-    return n1 + n2;
+    return resultado;
 }
 
-let devePrintar = true;
-let frase = 'O valor é: ';
-
-if(btn){
-    btn.addEventListener('click', () =>{
-        
-       if(input1 && input2){
-        console.log( somarNumeros(Number(input1.value),Number(input2.value),devePrintar, frase));
-       }
-       
-    }) 
-}
-
+// E aqui temos dois exemplos de uma chamada para essa função, um com strings e um com números
+console.log(juntar('bom ', 'dia'));
+console.log(juntar(1, 2));
